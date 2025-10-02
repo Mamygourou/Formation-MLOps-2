b420914a4bbc36cad5b93085cea77a4522cfa9e5
Ugo LE MORVAN <ugo.le-morvan@vinci-construction.com>
2025-10-02 07:12:07 +0000

    V2 for prediction

--- dags/predict.py
+++ dags/predict.py
@@ -8,7 +8,8 @@ from airflow.decorators import dag, task
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
 from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, PREDICTIONS_FOLDER, MODEL_PATH, FEATURES_PATH
-from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io, predict_with_io
+from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
+from formation_indus_ds_avancee.train_and_predict import predict_with_io
 
 
 @dag(default_args={'owner': 'airflow'}, schedule=timedelta(minutes=2),
