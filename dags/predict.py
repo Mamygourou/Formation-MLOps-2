95c2f1f3f479224a6efcf602dc4d5f3f7269dad7
Ugo LE MORVAN <ugo.le-morvan@vinci-construction.com>
2025-10-02 07:16:03 +0000

    Final version of predict

--- dags/predict.py
+++ dags/predict.py
@@ -7,9 +7,10 @@ from airflow.decorators import dag, task
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))  # So that airflow can find config files
 
-from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, PREDICTIONS_FOLDER, MODEL_PATH, FEATURES_PATH
+from dags.config import GENERATED_DATA_PATH, DATA_FOLDER, MODEL_PATH, PREDICTIONS_FOLDER
 from formation_indus_ds_avancee.feature_engineering import prepare_features_with_io
 from formation_indus_ds_avancee.train_and_predict import predict_with_io
+
 
 
 @dag(default_args={'owner': 'airflow'}, schedule=timedelta(minutes=2),
@@ -23,16 +24,14 @@ def predict():
                                  training_mode=False)
         return features_path
 
-    # Start completing predict task
     @task
-    def predict_with_io_task(feature_path):
-        predict_with_io(features_path = feature_path, model_path = MODEL_PATH, predictions_folder = PREDICTIONS_FOLDER)
-
-    # predict = PythonOperator()
-    # End completing predict task
+    def predict_with_io_task(feature_path: str) -> None:
+        predict_with_io(features_path=feature_path,
+                        model_path=MODEL_PATH,
+                        predictions_folder=PREDICTIONS_FOLDER)
 
     feature_path = prepare_features_with_io_task()
-    predict_with_io_task(feature_path)
+    predict_with_io_task(feature_path=feature_path)
 
 
 predict_dag = predict()
